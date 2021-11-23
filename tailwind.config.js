e9335df7c34c38a3a936aa7bf65dfef680518c89
Kenn Stack <kibadikennedy@gmail.com>
2021-11-24 00:20:30 +0800

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        sans: [
+          "Gontserrat",
+          "Montserrat",
+          "Roboto",
+          "bitter",
+          "Momcake",
+          "Open Sans",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
       colors: {
         "dark-pink": "#820552",
       },
