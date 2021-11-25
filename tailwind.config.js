40a9f1aea1a875bf219b0de54240a76122b23549
Kenn Stack <kibadikennedy@gmail.com>
2021-11-25 23:38:19 +0800

    added a cdn link for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        dpink: "#820552",
-        brand: "#820552",
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
